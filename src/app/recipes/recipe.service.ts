5a9a740ebdc0260e4c6e2b1c78c1310166244ef3
Vahan Asryan <vahan.asryan@avetiq.de>
2019-03-02 19:36:44 +0400

    Add ingredients in a recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   selectedRecipe = new EventEmitter<Recipe>();
@@ -10,16 +11,19 @@ export class RecipeService {
       'A test Recipe1',
       'Long description1',
       'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20),
+      ],
     ),
     new Recipe(
       'A test Recipe2',
       'Long description2',
       'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d',
-    ),
-    new Recipe(
-      'A test Recipe3',
-      'Long description3',
-      'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d',
+      [
+        new Ingredient('Meat2', 2),
+        new Ingredient('French Fries2', 30),
+      ],
     ),
   ];
 
